8962493fa00a67d6e97c2f4bf0e24058e0bdb477
Gabriel Brito de França <gabrielbfranca@gmail.com>
2025-09-02 23:20:16 -0300

    removendo dado sujo

--- script.js
+++ script.js
@@ -6,34 +6,6 @@ const CSV =
     attribution: "&copy; OpenStreetMap contributors",
   }).addTo(map);
 
-var data = [
-  {
-    Name: "Escola Quilombo 1",
-    Latitude: -15.82,
-    Longitude: -47.95,
-    Address: "Rua A, Cidade X",
-    Description: "Descrição da escola 1",
-  },
-  {
-    Name: "Escola Quilombo 2",
-    Latitude: -16.0,
-    Longitude: -48.05,
-    Address: "Rua B, Cidade Y",
-    Description: "Descrição da escola 2",
-  },
-];
-
-// Add markers with popups (cards)
-data.forEach(function (item) {
-  var marker = L.marker([item.Latitude, item.Longitude]).addTo(map);
-  marker.bindPopup(
-    "<b>" + item.Name + "</b><br>" + item.Address + "<br>" + item.Description
-  );
-});
-
-// Add search control
-L.Control.geocoder().addTo(map);
-
 // Example function to parse and rename columns
 function parseAndFixCsv(csvText) {
   // Split lines and get headers
